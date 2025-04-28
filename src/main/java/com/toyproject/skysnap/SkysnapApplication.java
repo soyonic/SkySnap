61e887285177aa5cdf8e55869de52da1fd203597
soyonic <soyonic00@gmail.com>
2025-04-28 17:18:43 +0900

    Project init

---
+++ src/main/java/com/toyproject/skysnap/SkysnapApplication.java
@@ -0,0 +1,13 @@
+lpackage com.toyproject.skysnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkysnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkysnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyproject/skysnap/SkysnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.skysnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkysnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
