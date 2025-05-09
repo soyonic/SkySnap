42f367cfa7c4a2cf21adc9e00cee6a969a94f664
MinEquals0 <minequals0@gmail.com>
2025-05-09 19:33:08 +0900

    chore: Api key 추가

---
+++ src/main/java/com/toyproject/skysnap/airport/controller/AirportController.java
@@ -0,0 +1,26 @@
+package com.toyproject.skysnap.airport.controller;
+
+import com.toyproject.skysnap.airport.dto.AirportDto;
+import com.toyproject.skysnap.airport.service.AirportService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/airports")
+public class AirportController {
+
+    private final AirportService airportService;
+
+    public AirportController(AirportService airportService) {
+        this.airportService = airportService;
+    }
+
+    @GetMapping
+    public List<AirportDto> getAirports(@RequestParam String city) {
+        return airportService.getAirportsByCity(city);
+    }
+}
