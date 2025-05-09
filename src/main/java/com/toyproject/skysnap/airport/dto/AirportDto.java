4886f61a51dbeea888604ed13c719d4fdea4710c
MinEquals0 <minequals0@gmail.com>
2025-05-09 19:33:56 +0900

    chore: Api key 추가

---
+++ src/main/java/com/toyproject/skysnap/airport/dto/AirportDto.java
@@ -0,0 +1,19 @@
+package com.toyproject.skysnap.airport.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+// 사용자에게 줄 DTO
+@Getter
+@Setter
+@AllArgsConstructor
+public class AirportDto {
+    private String airportName;
+    private String iataCode;
+    private String icaoCode;
+    private double latitude;
+    private double longitude;
+    private String countryName;
+    private String cityIataCode;
+}
