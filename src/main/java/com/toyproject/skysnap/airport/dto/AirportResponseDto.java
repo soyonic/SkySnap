1c727174d048aded1374d6de3fe82cac3f39585f
MinEquals0 <minequals0@gmail.com>
2025-05-10 15:01:37 +0900

    fix: AirportResponseDto @JsonProperty이용 매핑, CountryDto로 수정

--- src/main/java/com/toyproject/skysnap/airport/dto/AirportResponseDto.java
+++ src/main/java/com/toyproject/skysnap/airport/dto/AirportResponseDto.java
@@ -1,6 +1,8 @@
 package com.toyproject.skysnap.airport.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,20 @@ import lombok.Setter;
 @NoArgsConstructor
 public class AirportResponseDto {
     private String name; // 공항 이름
+
+    @JsonProperty("iata_code")
     private String iata;
+
+    @JsonProperty("icao_code")
     private String icao;
+    @JsonProperty("municipality")
     private String city;
-    private String country;
-    private Double latitude;  // null 가능성 고려해 Double 사용
+
+    private CountryDto country;
+
+    @JsonProperty("latitude_deg") // null 일 때를 고려해 Double 사용
+    private Double latitude;
+
+    @JsonProperty("longitude_deg")
     private Double longitude;
 }
